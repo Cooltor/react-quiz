47499242f425c68767926803d1a0a430ae0b47ce
Cooltor <romaindbus86@gmail.com>
2023-09-14 12:02:30 +0200

    dispatch "start"

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,8 @@
 function Header() {
   return (
-    <header className='app-header'>
-      <img src='logo512.png' alt='React logo' />
-      <h1>The React Quiz</h1>
+    <header className="app-header">
+      <img src="logokapa.png" alt="React logo" />
+      <h1>Le Quizz des Nullos</h1>
     </header>
   );
 }
